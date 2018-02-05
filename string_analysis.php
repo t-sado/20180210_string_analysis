f7aa95765f827eba1a4bbd4b1a0e854a635385f1
sado <sado.tomoki@no1s.biz>
2018-02-05 16:30:54 +0900

    リファクタリング

--- string_analysis.php
+++ string_analysis.php
@@ -4,6 +4,7 @@
 	$text = file_get_contents('data.txt');
 	$results = $igo->parse($text);
 
+	// 名詞一覧を取得する
 	$noun_list = array();
 	$sort_key = array();
 	foreach ($results as $result) {
@@ -11,9 +12,10 @@
 			$noun_list[$result->surface] = (isset($noun_list[$result->surface])) ? $noun_list[$result->surface] + 1 : 1;
 		}
 	}
-	foreach ($noun_list as $noun) {
-		$sort_key[] = $noun;
+	foreach ($noun_list as $count) {
+		$sort_key[] = $count;
 	}
+	// 出現回数で降順ソート
 	array_multisort($sort_key, SORT_DESC, $noun_list);
 	foreach ($noun_list as $noun => $count) {
 		echo str_pad($noun, 18, '　') . $count . "回\n";
